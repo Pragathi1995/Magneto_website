bba810b964cc5f0f6b29491c842a31b8346666c1
Pragathi.V <Pragathi.V@Pragathi>
2020-05-02 10:40:20 +0530

    second commit

--- Magneto_website/src/test/java/MagentoTest.java
+++ Magneto_website/src/test/java/MagentoTest.java
@@ -3,15 +3,14 @@
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class MagentoTest {
 	@Test
 	public void PostiveCre1() {
 		// TODO Auto-generated method stub
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
+		WebDriver driver=new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.get("https://www.magento.com");
 		
